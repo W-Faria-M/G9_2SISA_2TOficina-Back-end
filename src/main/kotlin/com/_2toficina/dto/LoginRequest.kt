9bab89365e93abebf34eaa9984b48d2a29c9574e
W-Faria-M <gabriel.barranco@sptech.school>
2025-04-26 04:36:30 -0300

    Add: implementa as classes de DTO que servem para autenticar o login e retornar seu resultado

---
+++ src/main/kotlin/com/_2toficina/dto/LoginRequest.kt
@@ -0,0 +1,6 @@
+package com._2toficina.dto
+
+data class LoginRequest(
+    val email: String,
+    val senha: String
+)
---
+++ src/main/kotlin/com/_2toficina/dto/RespostaLogin.kt
@@ -0,0 +1,6 @@
+package com._2toficina.dto
+
+data class RespostaLogin(
+    val usuarioId: Int?,
+    val logado: Boolean,
+)
