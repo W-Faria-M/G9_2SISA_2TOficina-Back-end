d8ce47cad84c393bd9b920a4bea844417817ca6e
W-Faria-M <gabriel.barranco@sptech.school>
2025-04-26 04:17:54 -0300

    Update: edita a formatação de resposta para a busca de usuários

--- src/main/kotlin/com/_2toficina/dto/RespostaUsuario.kt
+++ src/main/kotlin/com/_2toficina/dto/RespostaUsuario.kt
@@ -1,16 +1,9 @@
 package com._2toficina.dto
 
-import java.time.LocalDate
-import java.time.LocalDateTime
-
 data class RespostaUsuario(
     val id: Int,
-    val enderecoId: Int?,
-    val tipo: TipoUsuarioEnum,
-    val nome: String,
+    val tipoUsuario: Int,
+    val nomeCompleto: String,
     val telefone: String?,
     val email: String,
-    val dataCadastro: LocalDateTime,
-    val sexo: String?,
-    val dataNascimento: LocalDate?
 )
