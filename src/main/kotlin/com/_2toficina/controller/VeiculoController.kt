2423b115b0bcdb9f5f2d567f628cba9c399c5ef1
W-Faria-M <gabriel.barranco@sptech.school>
2025-06-11 13:48:43 -0300

    Refactor: Refatora a lógica do método atualizar km para atualizar todos os campos necessários

--- src/main/kotlin/com/_2toficina/controller/VeiculoController.kt
+++ src/main/kotlin/com/_2toficina/controller/VeiculoController.kt
@@ -13,15 +13,13 @@ class VeiculoController (
 ) {
     @GetMapping
     @Operation(summary = "Lista todos os veículos ou filtra por cliente.",
-        description = """
-        Retorna uma lista com os veículos cadastrados.
-        Se o parâmetro 'fkUsuario' for informado, retorna apenas os veículos desse usuário.
-        """)
-    fun listarVeiculos(@RequestParam(required = false) fkUsuario: Int?): ResponseEntity<List<Veiculo>> {
-        val veiculos = if (fkUsuario == null) {
+        description = """Retorna uma lista com os veículos cadastrados.
+        Se o parâmetro 'fkUsuario' for informado, retorna apenas os veículos desse usuário.""")
+    fun listarVeiculos(@RequestParam(required = false) usuarioId: Int?): ResponseEntity<List<Veiculo>> {
+        val veiculos = if (usuarioId == null) {
             veiculoRepository.findAll()
         } else {
-            veiculoRepository.findByFkUsuario(fkUsuario)
+            veiculoRepository.findByUsuarioId(usuarioId)
         }
 
         return if (veiculos.isEmpty()) {
@@ -43,7 +41,7 @@ class VeiculoController (
     @Operation(summary = "Atualiza um veículo existente.",
         description = "Retorna status 200 com o veículo atualizado ou status 404 se o veículo não for encontrado.")
     fun atualizarVeiculo(@PathVariable id: Int, @RequestBody veiculoAtualizado: Veiculo): ResponseEntity<Veiculo> {
-        return if (veiculoRepository.existsById(veiculoAtualizado.id)) {
+        return if (veiculoRepository.existsById(id)) {
             veiculoAtualizado.id = id
             val veiculoSalvo = veiculoRepository.save(veiculoAtualizado)
             ResponseEntity.status(200).body(veiculoSalvo)
@@ -52,19 +50,24 @@ class VeiculoController (
         }
     }
 
-    @PatchMapping("/atualizar-km/{id}")
-    @Operation(summary = "Atualiza a quilometragem de um veículo.",
-        description = "Retorna status 200 com o veículo atualizado ou status 404 se o veículo não for encontrado.")
-    fun atualizarKm(@PathVariable id: Int, @RequestParam km: Double): ResponseEntity<Veiculo> {
-        val veiculoOptional = veiculoRepository.findById(id)
-        return if (veiculoOptional.isPresent) {
-            val veiculo = veiculoOptional.get()
-            veiculo.km = km
-            val veiculoSalvo = veiculoRepository.save(veiculo)
-            ResponseEntity.status(200).body(veiculoSalvo)
-        } else {
-            ResponseEntity.status(404).build()
-        }
+    @PatchMapping("/atualizar-campo/{id}")
+    @Operation(summary = "Atualiza um ou mais campos de um veículo específico.",
+        description = "Retorna status 200 se algum campo for atualizado ou status 404 se o veículo não for encontrado.")
+    fun atualizarCampoVeiculo(@PathVariable id: Int, @RequestBody req: Veiculo): ResponseEntity<Void> {
+        val veiculoOpt = veiculoRepository.findById(id)
+        if (veiculoOpt.isEmpty) return ResponseEntity.status(404).build()
+
+        var veiculo = veiculoOpt.get()
+
+        veiculo = veiculo.copy(
+            placa = req.placa ?: veiculo.placa,
+            marca = req.marca ?: veiculo.marca,
+            modelo = req.modelo ?: veiculo.modelo,
+            ano = req.ano ?: veiculo.ano,
+            km = req.km ?: veiculo.km,
+        )
+        veiculoRepository.save(veiculo)
+        return ResponseEntity.status(200).build()
     }
 
     @DeleteMapping("/{id}")
