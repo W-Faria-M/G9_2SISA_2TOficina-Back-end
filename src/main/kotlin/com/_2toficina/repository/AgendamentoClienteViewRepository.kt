d74696ddce5ad461a35f8902498e32080d9649ea
W-Faria-M <gabriel.barranco@sptech.school>
2025-11-21 23:50:47 -0300

    chore: ajustes paroquiais

--- src/main/kotlin/com/_2toficina/repository/AgendamentoClienteViewRepository.kt
+++ src/main/kotlin/com/_2toficina/repository/AgendamentoClienteViewRepository.kt
@@ -4,5 +4,7 @@ import com._2toficina.entity.AgendamentoClienteView
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface AgendamentoClienteViewRepository: JpaRepository<AgendamentoClienteView, Int> {
+
     fun findByUsuarioId(usuarioId: Int): List<AgendamentoClienteView>
+
 }
