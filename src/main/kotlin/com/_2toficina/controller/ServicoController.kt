dd8328f7627927d67248f43ed1c72803a9a22347
W-Faria-M <gabriel.barranco@sptech.school>
2025-11-21 23:48:47 -0300

    add: adiciona novos endpoints para registrar novas categorias, e editar categorias e serviços cadastrados, e para retornar os dados da nova view implementada

--- src/main/kotlin/com/_2toficina/controller/ServicoController.kt
+++ src/main/kotlin/com/_2toficina/controller/ServicoController.kt
@@ -1,12 +1,16 @@
 package com._2toficina.controller
 
+import com._2toficina.entity.CategoriaServico
 import com._2toficina.entity.Servico
+import com._2toficina.entity.ServicosCompletosView
 import com._2toficina.entity.ServicosResumidosView
 import com._2toficina.entity.VeiculosClienteView
 import com._2toficina.repository.CategoriaServicoRepository
 import com._2toficina.repository.ServicoAgendadoRepository
 import com._2toficina.repository.ServicoRepository
+import com._2toficina.repository.ServicosCompletosViewRepository
 import com._2toficina.repository.ServicosResumidosViewRepository
+import com._2toficina.repository.StatusServicoRepository
 import com._2toficina.repository.VeiculosClienteViewRepository
 import io.swagger.v3.oas.annotations.Operation
 import org.apache.commons.lang3.mutable.Mutable
@@ -20,7 +24,9 @@ class ServicoController (
     private val servicoRepository: ServicoRepository,
     private val categoriaServicoRepository: CategoriaServicoRepository,
     private val servicoAgendadoRepository: ServicoAgendadoRepository,
-    private val servicosResumidosViewRepository: ServicosResumidosViewRepository
+    private val servicosResumidosViewRepository: ServicosResumidosViewRepository,
+    private val statusServicoRepository: StatusServicoRepository,
+    private val servicosCompletosViewRepository: ServicosCompletosViewRepository
 ){
 
     @GetMapping
@@ -45,16 +51,72 @@ class ServicoController (
     }
 
     @GetMapping("/resumidos")
-    @Operation(summary = "Retorna uma lista resumida com todos os serviços cadastrados.",
+    @Operation(summary = "Retorna uma lista resumida de todos os serviços cadastrados para seleção ao agendar.",
         description = """Retorna status 200 com a lista de serviços, ou
             status 204 se não houver serviços encontrados.""")
     fun listarServicosAgendamentos(): ResponseEntity<List<ServicosResumidosView>> {
-        val servicos = servicosResumidosViewRepository.findAll()
+        val servicos = servicosResumidosViewRepository.findByStatus(1)
         return if (servicos.isEmpty()) {
             ResponseEntity.status(204).build()
         } else {
             ResponseEntity.status(200).body(servicos)
         }
+    }
+
+    @GetMapping("/completos")
+    @Operation(summary = "Retorna uma lista completa com os serviços cadastrados para gerenciamento.",
+        description = """Retorna status 200 com a lista de serviços, ou
+            status 204 se não houver serviços encontrados.""")
+    fun listarServicosOficina(): ResponseEntity<List<ServicosCompletosView>> {
+        val servicos = servicosCompletosViewRepository.findAll()
+        return if (servicos.isEmpty()) {
+            ResponseEntity.status(204).build()
+        } else {
+            ResponseEntity.status(200).body(servicos)
+        }
+    }
+
+    @GetMapping("/categorias")
+    @Operation(summary = "Retorna uma lista com o id e nome de todas as categorias cadastradas para gerenciamento.",
+        description = """Retorna status 200 com a lista de categorias, ou
+            status 204 se não houver categorias encontradas.""")
+    fun listarCategorias(): ResponseEntity<List<CategoriaServico>> {
+        val categorias = categoriaServicoRepository.findAll()
+        return if (categorias.isEmpty()) {
+            ResponseEntity.status(204).build()
+        } else {
+            ResponseEntity.status(200).body(categorias)
+        }
+    }
+
+    @PostMapping("/categorias")
+    @Operation(
+        summary = "Cria uma nova categoria de serviço.",
+        description = "Retorna 201 com a categoria criada."
+    )
+    fun criarCategoria(@RequestBody novaCategoria: CategoriaServico): ResponseEntity<CategoriaServico> {
+        val categoriaSalva = categoriaServicoRepository.save(novaCategoria)
+        return ResponseEntity.status(201).body(categoriaSalva)
+    }
+
+    @PutMapping("/categorias/{id}")
+    @Operation(
+        summary = "Atualiza completamente o nome de uma categoria.",
+        description = "Retorna 200 se atualizada, ou 404 se não encontrada."
+    )
+    fun atualizarCategoria(
+        @PathVariable id: Int,
+        @RequestBody req: CategoriaServico
+    ): ResponseEntity<CategoriaServico> {
+        val categoriaOpt = categoriaServicoRepository.findById(id)
+        if (categoriaOpt.isEmpty) return ResponseEntity.status(404).build()
+
+        val categoria = categoriaOpt.get().copy(
+            nome = req.nome
+        )
+
+        val categoriaAtualizada = categoriaServicoRepository.save(categoria)
+        return ResponseEntity.status(200).body(categoriaAtualizada)
     }
 
     @PostMapping
@@ -88,7 +150,14 @@ class ServicoController (
                 },
             nome = req.nome ?: servico.nome,
             descricao = req.descricao ?: servico.descricao,
-            ehRapido = req.ehRapido ?: servico.ehRapido
+            ehRapido = req.ehRapido ?: servico.ehRapido,
+            statusServico =
+                if (req.statusServico != null) {
+                    statusServicoRepository.findById(req.statusServico!!.id)
+                        .orElseThrow { IllegalArgumentException("Status não encontrado") }
+                } else {
+                    servico.statusServico
+                }
         )
 
         servicoRepository.save(servico)
