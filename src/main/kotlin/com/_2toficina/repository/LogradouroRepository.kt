c94f63fcd087da2427be6c51ba138bd3a1b85164
W-Faria-M <gabriel.barranco@sptech.school>
2025-11-21 23:42:50 -0300

    remove: exclui as tabelas endereco e logradouro e toda lógica relacionada às mesmas

--- src/main/kotlin/com/_2toficina/repository/LogradouroRepository.kt
+++
@@ -1,8 +0,0 @@
-package com._2toficina.repository
-
-import com._2toficina.entity.Logradouro
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface LogradouroRepository: JpaRepository<Logradouro, Int> {}
